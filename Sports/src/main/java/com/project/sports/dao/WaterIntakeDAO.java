d01f56ecedb26566a6ff38f6c80a5e8a33eb3020
박주영 <@DESKTOP-1S9H41U>
2021-10-04 11:05:21 +0900

    chore : 물섭취량 달력 추가 fix : 달력 형식 변경(한범님 달력)

--- Sports/src/main/java/com/project/sports/dao/WaterIntakeDAO.java
+++ Sports/src/main/java/com/project/sports/dao/WaterIntakeDAO.java
@@ -4,10 +4,16 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.project.sports.domain.WaterIntake;
+
 @Repository
 public class WaterIntakeDAO {
 
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+
+	public void Waterinsert(WaterIntake water) {
+		sqlSession.insert("Water.insert", water);
+	}
 	
 }
